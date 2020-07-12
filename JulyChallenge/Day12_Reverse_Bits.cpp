5c022a1be9dc5e46c5bae2565f90cc1bc8911b94
Deepak Chinthada <chinthadadeepak@gmail.com>
2020-07-12 22:11:16 +0530

    Create Day12_Reverse_Bits.cpp

---
+++ JulyChallenge/Day12_Reverse_Bits.cpp
@@ -0,0 +1,43 @@
+/*
+
+Reverse bits of a given 32 bits unsigned integer.
+
+ 
+
+Example 1:
+
+Input:        00000010100101000001111010011100
+Output:       00111001011110000010100101000000
+Explanation:  The input binary string 00000010100101000001111010011100 represents the unsigned integer 43261596, 
+              so return 964176192 which its binary representation is 00111001011110000010100101000000.
+
+
+Example 2:
+
+Input:        11111111111111111111111111111101
+Output:       10111111111111111111111111111111
+Explanation:  The input binary string 11111111111111111111111111111101 represents the unsigned integer 4294967293,
+              so return 3221225471 which its binary representation is 10111111111111111111111111111111.
+ 
+ 
+ */
+ 
+ //Code:
+ 
+ class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+         bitset<32>b = {n};  //changes our input number to bits
+         int l=0,r=31;
+        while(l<r)
+        {
+            int k=b[l];
+            b[l]=b[r];
+            b[r]=k;
+            l++;
+            r--;
+        }
+        return b.to_ulong();  //typecast our bitset to unsigned long and return 
+        
+    }
+};
