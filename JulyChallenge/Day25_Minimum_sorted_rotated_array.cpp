8ad5b2d931ec82be726f694e201a212540a942e1
Deepak Chinthada <chinthadadeepak@gmail.com>
2020-07-26 00:15:05 +0530

    Create Day25_Minimum_sorted_rotated_array.cpp

---
+++ JulyChallenge/Day25_Minimum_sorted_rotated_array.cpp
@@ -0,0 +1,30 @@
+/*
+
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e.,  [0,1,2,4,5,6,7] might become  [4,5,6,7,0,1,2]).
+
+Find the minimum element.
+
+The array may contain duplicates.
+
+Example 1:
+
+Input: [1,3,5]
+Output: 1
+Example 2:
+
+Input: [2,2,2,0,1]
+Output: 0
+
+*/
+
+//Code:
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        return nums[0];
+    }
+};
