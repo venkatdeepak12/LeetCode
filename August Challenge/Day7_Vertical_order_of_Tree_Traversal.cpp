b9fb286a503283f6dd5f2cf62d86f4a625af5c64
Deepak Chinthada <chinthadadeepak@gmail.com>
2020-08-07 15:18:52 +0530

    Create Day7_Vertical_order_of_Tree_Traversal.cpp

---
+++ August Challenge/Day7_Vertical_order_of_Tree_Traversal.cpp
@@ -0,0 +1,66 @@
+/*
+
+Given a binary tree, return the vertical order traversal of its nodes values.
+
+For each node at position (X, Y), its left and right children respectively will be at positions (X-1, Y-1) and (X+1, Y-1).
+
+Running a vertical line from X = -infinity to X = +infinity, whenever the vertical line touches some nodes, we report the values of the nodes in order from top to bottom (decreasing Y coordinates).
+
+If two nodes have the same position, then the value of the node that is reported first is the value that is smaller.
+
+Return an list of non-empty reports in order of X coordinate.  Every report will have a list of values of nodes.
+
+ 
+
+Example 1:
+
+Input: [3,9,20,null,null,15,7]
+Output: [[9],[3,15],[20],[7]]
+Explanation: 
+Without loss of generality, we can assume the root node is at position (0, 0):
+Then, the node with value 9 occurs at position (-1, -1);
+The nodes with values 3 and 15 occur at positions (0, 0) and (0, -2);
+The node with value 20 occurs at position (1, -1);
+The node with value 7 occurs at position (2, -2).
+
+
+Example 2:
+
+Input: [1,2,3,4,5,6,7]
+Output: [[4],[2],[1,5,6],[3],[7]]
+Explanation: 
+The node with value 5 and the node with value 6 have the same position according to the given scheme.
+However, in the report "[1,5,6]", the node value of 5 comes first since 5 is smaller than 6.
+
+
+*/
+
+//Code:
+
+class Solution {
+public:
+    void print(TreeNode* root,int h,map<int,map<int,vector<int>>>& mp,int level)
+    {
+        if(!root) return;
+        mp[h][level].push_back(root->val);     // insert them according to level
+        print(root->left,h-1,mp,level+1);
+        print(root->right,h+1,mp,level+1);  
+    }
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        vector<vector<int>> ans;
+        if(!root) return ans;
+        map<int,map<int,vector<int>>> mp;
+        print(root,0,mp,0);
+        for(auto i:mp)
+        {
+            vector<int> v;
+            for(auto j:i.second)
+            {
+               sort(j.second.begin(), j.second.end());     // we are sorting if two values are at same level and in same line we print in ascendinng order
+               v.insert(v.end(), j.second.begin(), j.second.end());
+            }
+            ans.push_back(v);
+        }
+        return ans;
+    }
+};
