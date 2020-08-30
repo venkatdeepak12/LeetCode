21b18f63b1bce5885cd5ed2ecdc1fb707aea8891
Deepak Chinthada <chinthadadeepak@gmail.com>
2020-08-30 22:18:33 +0530

    Create Day30_Largest_Component_Size_By_Common_Factor.cpp

---
+++ August Challenge/Day30_Largest_Component_Size_By_Common_Factor.cpp
@@ -0,0 +1,75 @@
+/*
+
+Given a non-empty array of unique positive integers A, consider the following graph:
+
+There are A.length nodes, labelled A[0] to A[A.length - 1];
+There is an edge between A[i] and A[j] if and only if A[i] and A[j] share a common factor greater than 1.
+Return the size of the largest connected component in the graph.
+
+ 
+
+Example 1:
+
+Input: [4,6,15,35]
+Output: 4
+
+Example 2:
+
+Input: [20,50,9,63]
+Output: 2
+
+Example 3:
+
+Input: [2,3,6,7,4,12,21,39]
+Output: 8
+
+
+*/
+
+//Code:
+
+class UnionFindSet {
+public:
+    UnionFindSet(int n) : _parent(n) {
+        for (int i=0; i<n; i++) {
+            _parent[i] = i;
+        }
+    }
+    
+    void Union(int x, int y) {
+        _parent[Find(x)] = _parent[Find(y)];
+    }
+    
+    int Find(int x) {
+        if (_parent[x] != x) {
+            _parent[x] = Find(_parent[x]);
+        }
+        return _parent[x];
+    }
+    
+private:
+    vector<int> _parent;
+};
+
+class Solution {
+public:
+    int largestComponentSize(vector<int>& A) {
+        int n = *max_element(A.begin(), A.end());
+        UnionFindSet ufs(n+1);
+        for (int &a : A) {
+            for (int k = 2; k <= sqrt(a); k++) {
+                if (a % k == 0) {
+                    ufs.Union(a, k);
+                    ufs.Union(a, a / k);
+                }
+            }
+        }
+        
+        unordered_map<int, int> mp;
+        int ans = 1;
+        for (int &a : A) {
+            ans = max(ans, ++mp[ufs.Find(a)]);
+        }
+        return ans;
+    }
+};
